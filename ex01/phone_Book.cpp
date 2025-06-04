0fd877539fdee6788306a58359df2c1359636470
daniel <daniel-congo2@hotmail.com>
2025-06-04 17:21:27 +0200

    phoneBook en cours

--- ex01/phone_Book.cpp
+++ ex01/phone_Book.cpp
@@ -6,14 +6,15 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 21:36:15 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/06/03 15:18:52 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/06/04 17:18:35 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 class Contact {
-	
+	public:
+	std
 };
 
 class PhoneBook {
@@ -21,8 +22,22 @@ class PhoneBook {
 	Contact str[8];
 };
 
+void	add_contact(void)
+{
+	std::string name;
+	// std::string surname;
+	// std::string nickname;
+	// std::string phone;
+	// std::string secret;
+
+	std::cout << "first name: ";
+	std::cin >> name;
+	std::cout << name << "\n";
+}
 int	main(int argc, char **argv)
 {
-	
+	std::string add = "ADD";
+	if (argc == 2 && argv[1] == add)
+		add_contact();
 	return (0);
 }
